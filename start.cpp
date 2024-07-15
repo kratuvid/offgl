565c3c3c4e4440c5768f81a9c1164e5536fa2b88
Mantresh <ray779@tuta.io>
2024-07-15 10:00:29 +0530

    using glad

--- start.cpp
+++ start.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cstring>
 
+#include <glad/glad.h>
 #include <GL/osmesa.h>
 #include <png.h>
 
@@ -12,21 +13,25 @@ using namespace std::literals::string_literals;
 class Container
 {
 public:
+	void run()
+	{
+		std::memset(buffer, 0x00, buffer_size);
+
+		glClearColor(0, 0, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glFinish();
+	}
 	void write()
 	{
 		auto rowsp = new png_bytep[height];
 		for (int j : std::views::iota(0, height))
-			rowsp[j] = static_cast<png_bytep>(&buffer[j * width]);
+			rowsp[j] = reinterpret_cast<png_bytep>(&buffer32[j * width]);
 
 		png_set_rows(pngp, png_infop, rowsp);
 		png_write_png(pngp, png_infop, PNG_TRANSFORM_IDENTITY, nullptr);
 
 		delete[] rowsp;
-	}
-
-	void run()
-	{
-		std::memset(buffer, 0xff, buffer_size);
 	}
 
 	Container(int width, int height, std::string filename)
@@ -48,7 +53,10 @@ public:
 		if (buffer)
 			delete[] buffer;
 		if (ctx)
+		{
+			OSMesaMakeCurrent(ctx, 0, 0, 0, 0);
 			OSMesaDestroyContext(ctx);
+		}
 	}
 
 	void init()
@@ -67,13 +75,16 @@ public:
 		if (!pngp)
 			throw "Unable to create the pngp"s;
 
+		if (setjmp(png_jmpbuf(pngp)))
+			throw "setjmp for libpng failed"s;
+
 		png_infop = png_create_info_struct(pngp);
 		if (!png_infop)
 			throw "Unable to create png_infop"s;
 
 		png_init_io(pngp, fp);
 
-		png_set_IHDR(pngp, png_infop, width, height, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+		png_set_IHDR(pngp, png_infop, width, height, 8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 	}
 
 	void init_gl()
@@ -82,6 +93,9 @@ public:
 		const int attrib_list[] {
 			OSMESA_FORMAT, OSMESA_RGBA,
 			OSMESA_PROFILE, OSMESA_CORE_PROFILE,
+			OSMESA_DEPTH_BITS, 0,
+			OSMESA_STENCIL_BITS, 0,
+			OSMESA_ACCUM_BITS, 0,
 			OSMESA_CONTEXT_MAJOR_VERSION, 4,
 			OSMESA_CONTEXT_MINOR_VERSION, 5,
 			0
@@ -95,9 +109,10 @@ public:
 		buffer = new GLubyte[buffer_size];
 
 		if (!OSMesaMakeCurrent(ctx, buffer, GL_UNSIGNED_BYTE, width, height))
-			throw "Failed making the context current"s;
+			throw "Failed to make the context current"s;
 
-		load_gl_functions();
+		if (gladLoadGLLoader((GLADloadproc)OSMesaGetProcAddress) == 0)
+			throw "Failed to load OpenGL function pointers"s;
 
 		// Stats
 		const std::pair<GLenum, const char*> query_list[] {
@@ -108,46 +123,27 @@ public:
 		};
 		for (auto [query, desc] : query_list)
 		{
-			std::println(stderr, "{}: {}", desc, (const char*)_glGetString(query));
+			std::println(stderr, "{}: {}", desc, (const char*)glGetString(query));
 		}
 
 		GLint nexts;
-		_glGetIntegerv(GL_NUM_EXTENSIONS, &nexts);
+		glGetIntegerv(GL_NUM_EXTENSIONS, &nexts);
 		if (nexts > 0)
 		{
 			std::println(stderr, "Discovered {} extensions", nexts);
 			if (false)
 			for (int i : std::views::iota(0, nexts))
 			{
-				std::println(stderr, "{}", (const char*)_glGetStringi(GL_EXTENSIONS, i));
+				std::println(stderr, "{}", (const char*)glGetStringi(GL_EXTENSIONS, i));
 			}
 		}
 
 		// Debug output
-		_glEnable(GL_DEBUG_OUTPUT);
-		_glDebugMessageCallback(debug_callback, this);
-	}
+		glEnable(GL_DEBUG_OUTPUT);
+		glDebugMessageCallback(debug_callback, this);
 
-	void load_gl_functions()
-	{
-		auto load = [](auto& pfn, const char* name) {
-			pfn = reinterpret_cast<std::remove_reference<decltype(pfn)>::type>(OSMesaGetProcAddress(name));
-			if (!pfn)
-				throw std::format("Couldn't obtain the address of function {}", name);
-			std::println(stderr, "Loaded function: {}: {}", name, (void*)pfn);
-		};
-
-#define _LOAD(name) load(_##name, #name)
-
-		_LOAD(glGetString);
-		_LOAD(glGetStringi);
-		_LOAD(glGetIntegerv);
-		_LOAD(glEnable);
-		_LOAD(glDisable);
-		_LOAD(glDebugMessageInsert);
-		_LOAD(glDebugMessageCallback);
-
-#undef _LOAD
+		// View
+		glViewport(0, 0, width, height);
 	}
 
 private:
@@ -161,7 +157,10 @@ private:
 	OSMesaContext ctx = nullptr;
 
 	int width, height;
-	GLubyte* buffer = nullptr;
+	union {
+		GLubyte* buffer;
+		uint32_t* buffer32 = nullptr;
+	};
 	size_t buffer_stride, buffer_size;
 
 	// libpng
@@ -169,15 +168,6 @@ private:
 	FILE* fp = nullptr;
 	png_structp pngp = nullptr;
 	png_infop png_infop = nullptr;
-
-private:
-	GLubyte* (*_glGetString) (GLenum name);
-	GLubyte* (*_glGetStringi) (GLenum name, GLuint index);
-	void (*_glGetIntegerv) (GLenum pname, GLint* data);
-	void (*_glEnable) (GLenum cap);
-	void (*_glDisable) (GLenum cap);
-	void (*_glDebugMessageInsert) (GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const char* message);
-	void (*_glDebugMessageCallback) (void (*debugproc) (GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* data), void* data);
 };
 
 int main(int argc, char** argv)
