eb60dc8241581966eeea1fe61f5fb5130332474b
Mantresh <ray779@tuta.io>
2024-07-15 12:07:51 +0530

    hello triangle

--- hello.cpp
+++ hello.cpp
@@ -8,25 +8,150 @@
 #include <GL/osmesa.h>
 #include <png.h>
 
+#define iassert(expr) if (!(expr)) throw std::format("Assertion `{}` failed", #expr)
+
 using namespace std::literals::string_literals;
 
 class Container
 {
+private:
+	GLuint shape_vbo, shape2_vbo, shape_vao, shape2_vao;
+	GLuint vs, fs, fs2, shader, shader2;
+
 public:
+	void init_user()
+	{
+		// Shaders
+		const char* vs_code {
+			R"(#version 400
+			in vec3 vertex;
+			in float offset;
+			void main() {
+				gl_Position = vec4(vertex + offset, 1.0);
+			})"
+		};
+
+		const char* fs_code {
+			R"(#version 400
+			out vec4 color;
+			void main() {
+				color = vec4(0.5, 0.0, 0.5, 1.0);
+			})"
+		};
+
+		vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, 1, &vs_code, nullptr);
+		glCompileShader(vs);
+
+		fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, 1, &fs_code, nullptr);
+		glCompileShader(fs);
+
+		shader = glCreateProgram();
+		glAttachShader(shader, vs);
+		glAttachShader(shader, fs);
+		glLinkProgram(shader);
+
+		const char* fs2_code {
+			R"(#version 400
+			out vec4 color;
+			void main() {
+				color = vec4(0.5, 0.5, 0.0, 1.0);
+			})"
+		};
+
+		fs2 = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs2, 1, &fs2_code, nullptr);
+		glCompileShader(fs2);
+
+		shader2 = glCreateProgram();
+		glAttachShader(shader2, vs);
+		glAttachShader(shader2, fs2);
+		glLinkProgram(shader2);
+
+		// Vertices
+		// Shape 1
+		const float vertices[] {
+			-0.5, 0.5, 0,
+			0.5, -0.5, 0,
+			-0.5, -0.5, 0,
+
+			0.5, 0.5, 0,
+			0.5, -0.5, 0,
+			-0.5, 0.5, 0
+		};
+
+		const float offsets[] {
+			0.25, 0.25, 0.25,
+			0.25, 0.25, 0.25
+		};
+
+		glGenBuffers(1, &shape_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, shape_vbo);
+		glNamedBufferStorage(shape_vbo, sizeof(vertices) + sizeof(offsets), nullptr, GL_DYNAMIC_STORAGE_BIT);
+		glNamedBufferSubData(shape_vbo, 0, sizeof(vertices), vertices);
+		glNamedBufferSubData(shape_vbo, sizeof(vertices), sizeof(offsets), offsets);
+
+		glGenVertexArrays(1, &shape_vao);
+		glBindVertexArray(shape_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, shape_vbo);
+
+		GLuint attrib_index;
+		attrib_index = glGetAttribLocation(shader, "vertex");
+		glEnableVertexArrayAttrib(shape_vao, attrib_index);
+		glVertexAttribPointer(attrib_index, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		attrib_index = glGetAttribLocation(shader, "offset");
+		glEnableVertexArrayAttrib(shape_vao, attrib_index);
+		glVertexAttribPointer(attrib_index, 1, GL_FLOAT, GL_FALSE, 0, (const void*)sizeof(vertices));
+
+		// Shape 2
+		const float offsets2[] {
+			-0.25, -0.25, -0.25,
+			-0.25, -0.25, -0.25
+		};
+
+		glGenBuffers(1, &shape2_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, shape2_vbo);
+		glNamedBufferStorage(shape2_vbo, sizeof(vertices) + sizeof(offsets2), nullptr, GL_DYNAMIC_STORAGE_BIT);
+		glNamedBufferSubData(shape2_vbo, 0, sizeof(vertices), vertices);
+		glNamedBufferSubData(shape2_vbo, sizeof(vertices), sizeof(offsets2), offsets2);
+
+		glGenVertexArrays(1, &shape2_vao);
+		glBindVertexArray(shape2_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, shape2_vbo);
+
+		attrib_index = glGetAttribLocation(shader2, "vertex");
+		glEnableVertexArrayAttrib(shape2_vao, attrib_index);
+		glVertexAttribPointer(attrib_index, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		attrib_index = glGetAttribLocation(shader2, "offset");
+		glEnableVertexArrayAttrib(shape2_vao, attrib_index);
+		glVertexAttribPointer(attrib_index, 1, GL_FLOAT, GL_FALSE, 0, (const void*)sizeof(vertices));
+	}
+
 	void run()
 	{
-		std::memset(buffer, 0x00, buffer_size);
+		glClearColor(0.6, 0.6, 0.8, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glClearColor(0, 0, 1, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shader);
+		glBindVertexArray(shape_vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glUseProgram(shader2);
+		glBindVertexArray(shape2_vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glFinish();
 	}
+
+public:
 	void write()
 	{
 		auto rowsp = new png_bytep[height];
 		for (int j : std::views::iota(0, height))
-			rowsp[j] = reinterpret_cast<png_bytep>(&buffer32[j * width]);
+			rowsp[height-j-1] = reinterpret_cast<png_bytep>(&buffer32[j * width]);
 
 		png_set_rows(pngp, png_infop, rowsp);
 		png_write_png(pngp, png_infop, PNG_TRANSFORM_IDENTITY, nullptr);
@@ -63,6 +188,7 @@ public:
 	{
 		init_gl();
 		init_libpng();
+		init_user();
 	}
 
 	void init_libpng()
@@ -93,7 +219,7 @@ public:
 		const int attrib_list[] {
 			OSMESA_FORMAT, OSMESA_RGBA,
 			OSMESA_PROFILE, OSMESA_CORE_PROFILE,
-			OSMESA_DEPTH_BITS, 0,
+			OSMESA_DEPTH_BITS, 24,
 			OSMESA_STENCIL_BITS, 0,
 			OSMESA_ACCUM_BITS, 0,
 			OSMESA_CONTEXT_MAJOR_VERSION, 4,
@@ -144,12 +270,16 @@ public:
 
 		// View
 		glViewport(0, 0, width, height);
+
+		// Depth testing
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
 	}
 
 private:
 	static void debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* data)
 	{
-		std::println(stderr, "Debug callback: source: {:#x}, type: {:#x}, severity: {:#x}: {}", source, type, severity, message);
+		std::println(stderr, "GL debug callback: source: {:#x}, type: {:#x}, severity: {:#x}: {}", source, type, severity, message);
 	}
 
 private:
